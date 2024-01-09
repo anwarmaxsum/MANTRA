af42a91ed7cabe102941302d83f02119906e5802
anwarmaxsum <13119856+anwarmaxsum@users.noreply.github.com>
2024-01-09 18:18:32 +1030

    Add files via upload

---
+++ code/latestscript/run_E3k_Weather_pl192.sh
@@ -0,0 +1,30 @@
+
+python -u run_dualmode3k.py \
+  --is_training 1 \
+  --root_path ./dataset/weather/ \
+  --data_path weather.csv \
+  --model_id Weather_E3k_96_192 \
+  --model B6autoformer \
+  --slow_model AutoformerS1 \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 192 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 21 \
+  --dec_in 21 \
+  --c_out 21 \
+  --des 'Exp' \
+  --itr 3 \
+  --n_learner 3 \
+  --urt_heads 1 \
+  --learning_rate 0.001 \
+  --dropout 0.1 \
+  --anomaly 1.0 \
+  --d_model 256 \
+  --fix_seed 2023 \
+  --checkpoints ./checkpoints1/
+
